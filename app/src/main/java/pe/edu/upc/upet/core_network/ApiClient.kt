ec6e4c07b03e01cea5a865a2f96ae1d3ffe99125
Marcelo rentería <u202216558@upc.edu.pe>
2025-11-11 01:03:32 -0500

    feat: Changed API link.

--- app/src/main/java/pe/edu/upc/upet/core_network/ApiClient.kt
+++ app/src/main/java/pe/edu/upc/upet/core_network/ApiClient.kt
@@ -2,5 +2,5 @@ package pe.edu.upc.upet.core_network;
 
 object ApiClient {
     //const val BASE_URL = "https://upetbackendapi.onrender.com/api/v1/"
-    const val BASE_URL = "https://web-production-4270c.up.railway.app/api/v1/";
+    const val BASE_URL = "https://upet-backend-production.up.railway.app/api/v1/";
 }
